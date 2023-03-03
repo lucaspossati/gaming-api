8332a404c1e679d3c87f568f8ffeb027a7dceef3
Lucas Davi Possati <lucasdavipossati@hotmail.com>
2023-03-03 01:25:52 -0300

    clear migrations

---
+++ Data/Migrations/20230303041453_First-migration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Firstmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    registrationdate = table.Column<DateTime>(name: "registration_date", type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    fullname = table.Column<string>(name: "full_name", type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    phonenumber = table.Column<string>(name: "phone_number", type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    address = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    companyid = table.Column<Guid>(name: "company_id", type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_People_Companies_company_id",
+                        column: x => x.companyid,
+                        principalTable: "Companies",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_company_id",
+                table: "People",
+                column: "company_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
